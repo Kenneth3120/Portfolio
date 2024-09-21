57fba58df39934126257a7ae1a1b03cfc1f7105a
Kenneth Rebello <kennethrebello253@gmail.com>
2024-09-21 10:01:03 +0530

    updated images

--- data/index.ts
+++ data/index.ts
@@ -86,7 +86,7 @@ export const navItems = [
       id: 2,
       title: "Yoom - Video Conferencing App",
       des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-      img: "/p2.svg",
+      img: "/parkx.svg",
       iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
       link: "https://github.com/adrianhajdin/zoom-clone",
     },
